697e833eafc0cf884935501e092be0f180281a80
Paulo Henrique <contato@paulohenriquesn.com>
2024-01-14 19:00:42 -0300

    docs: add readme.md

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='decorator-http',
-    version='0.0.1',
+    version='0.0.3',
     author='Paulo Henrique',
     author_email='contact@paulohenriquesn.com',
     description='A easy way to use requests but with decorators',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
